759a9de03a04953a2c1886df6290a89d83577bac
Asadhjafri <asadhjafri@gmail.com>
2021-06-23 22:45:06 -0400

    import link from react router and add in styled component for logo. Import browser as router in index js. Use router and wrapped App tag in Router tags.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 
 const Nav = styled.nav`
     height:60px;
     background:red;
+`;
 
-`
+const Logo = styled(Link)`
+    color:#fff;
+`;
 
-export const Navbar = () => {
+const MenuBars=styled.i`
+
+`;
+
+const NavMenu=styled.div`
+
+`;
+
+const Navbar = () => {
     return (
         <Nav>
+            <Logo>JDMarket</Logo>
+            <MenuBars />
+            <NavMenu></NavMenu>
             <h1>Navbar</h1>
         </Nav>
     )
